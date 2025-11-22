5a32583f3e8e72d429040e92d8856d9249351794
Md. Masud <Masud>
2025-11-22 20:23:59 +0600

    build a fully post screen

--- lib/view/note/note_screen.dart
+++ lib/view/note/note_screen.dart
@@ -11,7 +11,7 @@ class _NoteScreenState extends State<NoteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFD1AD9F),
+      backgroundColor: Color(0xFFD1AD9F),
       appBar: AppBar(
         backgroundColor: Color(0xFF795545),
         leading: Padding(
@@ -19,10 +19,7 @@ class _NoteScreenState extends State<NoteScreen> {
           child: CircleAvatar(
             backgroundColor: Colors.white,
 
-            child: Icon(
-                Icons.keyboard_backspace_outlined,
-              size: 20,
-            ),
+            child: Icon(Icons.keyboard_backspace_outlined, size: 25),
           ),
         ),
         actions: [
@@ -40,78 +37,160 @@ class _NoteScreenState extends State<NoteScreen> {
                 style: TextStyle(
                   color: Color(0xFF795545),
                   fontSize: 18,
-                  fontWeight: FontWeight.w600
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
 
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
 
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.black),
-          ),
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              color: Color(0xFFF3D7CC),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black),
+            ),
 
-          width: double.infinity,
-          height: 250,
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFF795545),
-                  borderRadius: BorderRadiusDirectional.only(
+            width: double.infinity,
+            height: 250,
+            child: Column(
+              children: [
+                // card top bar
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF795545),
+                    borderRadius: BorderRadiusDirectional.only(
                       topStart: Radius.circular(9),
-                    topEnd: Radius.circular(9)
-                  )
-                ),
-                height: 50,
-                width: double.infinity,
-                child: Row(
-                  spacing: 20,
-                  children: [
-                    //serial post number
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadiusGeometry.directional(
-                          topEnd: Radius.circular(20),
-                          bottomEnd: Radius.circular(20)
-                        )
-                      ),
-                      width: 50,
-                      height: 36,
-                      child: Center(
-                        child: Text(
-                          "১",
-                          style: TextStyle(
-                            color: Color(0xFF795545),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 25
+                      topEnd: Radius.circular(9),
+                    ),
+                  ),
+                  height: 50,
+                  width: double.infinity,
+                  child: Row(
+                    spacing: 20,
+                    children: [
+                      //serial post number
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadiusGeometry.directional(
+                            topEnd: Radius.circular(20),
+                            bottomEnd: Radius.circular(20),
+                          ),
+                        ),
+                        width: 50,
+                        height: 36,
+                        child: Center(
+                          child: Text(
+                            "১",
+                            style: TextStyle(
+                              color: Color(0xFF795545),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 25,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    // title text
-                    Text(
-                      "বিচার দিবস",
-                      style: TextStyle(
+                      // title text
+                      Text(
+                        "বিচার দিবস",
+                        style: TextStyle(
                           color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // card body
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEED5CE),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFFD1AD9F),
+                        blurRadius: 1,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                  height: 120,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "অন্যের জীবন নষ্ট করা মানুষগুলো অন্যায় করার সময় ভুলে যায় বিচারদিবসের মালিকের কথা!",
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          ),
-        ),
 
+                //bottom buttons
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //copy button
+                      CircleAvatar(
+                        backgroundColor: Color(0xFF795545),
+                        maxRadius: 20,
+                        child: Center(
+                          child: Icon(Icons.copy, color: Colors.white),
+                        ),
+                      ),
+
+                      // sear button
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                            Color(0xFF795545),
+                          ),
+                        ),
+                        child: Row(
+                          spacing: 10,
+                          children: [
+                            Icon(Icons.share, color: Colors.white, size: 20),
+                            Text(
+                              "শেয়ার করুন...",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        )
       ),
     );
   }
